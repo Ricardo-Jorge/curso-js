96e4d21717e071c55aa8fb196d09d6a99912e65b
Ricardo Jorge <ricardolemos18@hotmail.com>
2024-06-14 15:50:25 -0300

    teste

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
+require('dotenv').config();
+
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.CONNECTIONSTRING)
+    .then(() => {
+        console.log('Conectado a base de dados');
+        app.emit('pronto');
+    })
+    .catch(e => console.log(e));
+
 const routes = require('./routes');
 const path = require('path');
 
@@ -12,8 +23,10 @@ app.set('view engine', 'ejs');
 
 app.use(routes);
 
-app.listen(3000, () => {
-    console.log('Acessar http://localhost:3000');
-    console.log('Servidor executando na porta 3000')
+app.on('pronto', () => {
+    app.listen(3000, () => {
+        console.log('Acessar http://localhost:3000');
+        console.log('Servidor executando na porta 3000')
+    });
 });
 
