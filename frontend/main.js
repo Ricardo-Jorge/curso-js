d020847758f6e8b0d11eec5590bf87c1f5addb56
Ricardo Jorge <ricardolemos18@hotmail.com>
2024-06-27 17:18:54 -0300

    Validação FrontEnd

--- frontend/main.js
+++ frontend/main.js
@@ -1,5 +1,9 @@
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 
-import './assets/CSS/style.css';
+import Login from './modules/Login'
 
+const login = new Login('.form-login');
+const cadastro = new Login('.form-cadastro');
+login.init();
+cadastro.init();
